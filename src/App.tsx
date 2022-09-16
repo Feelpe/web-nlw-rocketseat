7971e9e1c4fda1bbcddda396c16b6e64bacfe8db
felipe <felipesscreator@gmail.com>
2022-09-16 12:11:28 -0300

    feat: modal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 
-import { GameBanner } from './components/GameBanner';
-import { CreateAdBanner } from './components/createAdBanner';
+import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/createAdBanner";
 
-import './styles/main.css';
+import "./styles/main.css";
 
-import logoImg from './assets/logo.svg';
+import logoImg from "./assets/logo.svg";
 
 interface Game {
   id: string;
@@ -13,44 +14,62 @@ interface Game {
   bannerUrl: string;
   _count: {
     ads: number;
-  }
+  };
 }
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-    .then(response => response.json())
-    .then(data => {
-      setGames(data);
-    })
-  }, [])
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
 
   return (
-    <div className='max-w-[1344px] mx-auto flex flex-col items-center m-20'>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center m-20 px-8">
       <img src={logoImg} alt="" />
 
-      <h1 className='text-6xl text-white font-black mt-20'>
-        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>Duo</span> está aqui.
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          Duo
+        </span>{" "}
+        está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'> 
-        {games.map(game => {
-          return(
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => {
+          return (
             <GameBanner
               key={game.id}
-              bannerUrl={game.bannerUrl} 
-              title={game.title} 
+              bannerUrl={game.bannerUrl}
+              title={game.title}
               adsCount={game._count.ads}
             />
-          )
+          );
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed"/>
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
+            <Dialog.Title>Publique um anúncio</Dialog.Title>
+
+            <Dialog.Content>
+              qualquer coisa
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
