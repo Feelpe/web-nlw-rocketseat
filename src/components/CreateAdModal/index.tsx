85d4cf2893909e30d782e43b0346888eaf649e84
felipe <felipesscreator@gmail.com>
2022-09-18 09:53:28 -0300

    feat: checkbox style

--- src/components/CreateAdModal/index.tsx
+++ src/components/CreateAdModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as CheckBox from "@radix-ui/react-checkbox";
 
-import { GameController } from "phosphor-react";
+import { Check, GameController } from "phosphor-react";
 
 import { Input } from "../Form/input";
 
@@ -8,12 +9,10 @@ export function CreateAdModal() {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-
       <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
         <Dialog.Title className="text-3xl font-black">
           Publique um anúncio
         </Dialog.Title>
-
         <form className="mt-8 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label htmlFor="game" className="font-semibold">
@@ -21,12 +20,10 @@ export function CreateAdModal() {
             </label>
             <Input id="game" placeholder="Selecione o game que deseja jogar" />
           </div>
-
           <div className="flex flex-col gap-2">
             <label htmlFor="">Seu nome (nickname)</label>
             <Input id="name" placeholder="Como te chamam dentro do game?" />
           </div>
-
           <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col gap-2">
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
@@ -41,7 +38,6 @@ export function CreateAdModal() {
               <Input id="discord" type="text" placeholder="Usuario#0000" />
             </div>
           </div>
-
           <div className="flex gap-6">
             <div className="flex flex-col gap-2">
               <label htmlFor="weekDays">Quando costuma jogar?</label>
@@ -78,12 +74,14 @@ export function CreateAdModal() {
               </div>
             </div>
           </div>
-
-          <div className="mt-2 flex gap-2 text-sm">
-            <Input type="checkbox" />
+          <div className="mt-2 flex items-center gap-2 text-sm">
+            <CheckBox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+              <CheckBox.Indicator>
+                <Check className="w-4 h-4 text-emerald-400" />
+              </CheckBox.Indicator>
+            </CheckBox.Root>
             Costumo a me conectar ao chat de boz
           </div>
-
           <footer className="mt-4 flex justify-end">
             <Dialog.Close
               type="button"
